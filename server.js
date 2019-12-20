d68a098ada56343bb82f389a09cb3da0879a0745
Fran <frapercan1@alum.us.es>
2019-12-20 09:25:24 +0100

    cambiado puerto a 9000

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(errorHandler);
 
  
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 9001;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 9000;
 const server = app.listen(port, function () {
     console.log('Server listening on port ' + port);
 });
